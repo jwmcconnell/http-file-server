6797751dc5250c5d6be0f50b28e3325aae88f305
Jackmc <jackmc1122@gmail.com>
2019-06-26 16:21:46 -0700

    Search for files in the public dir and if that file does not exist return a not found page.

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('application routes', () => {
-  it('returns the index page with a welcom header', () => {
+  it('returns the index page with a welcome header', () => {
     return request(app)
       .get('/index.html')
       .then(res => {
@@ -10,11 +10,19 @@ describe('application routes', () => {
       });
   });
 
-  it('returns the index page with a welcom header when there is an empty path', () => {
+  it('returns the index page with a welcome header when there is an empty path', () => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('<h1>Welcome</h1>'));
+      });
+  });
+
+  it('returns a not found page when the path does not match a file in public', () => {
+    return request(app)
+      .get('/badpath')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('<h1>Not Found</h1>'));
       });
   });
 });
