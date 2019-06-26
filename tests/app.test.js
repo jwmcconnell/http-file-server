36dec238249f1e31e13960b318f42bee207dc95e
Jackmc <jackmc1122@gmail.com>
2019-06-26 16:08:31 -0700

    Add a route for an empty path to take you to the index.html page.

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,4 +9,12 @@ describe('application routes', () => {
         expect(res.text).toEqual(expect.stringContaining('<h1>Welcome</h1>'));
       });
   });
+
+  it('returns the index page with a welcom header when there is an empty path', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('<h1>Welcome</h1>'));
+      });
+  });
 });
